3bb93002161df85cd5eaa8d98c9c8c082fbfc97b
JC <keitokuch@gmail.com>
2022-06-23 22:48:20 -0400

    Fix jget update dir name

--- jget_lib/formula/jget.sh
+++ jget_lib/formula/jget.sh
@@ -1,5 +1,5 @@
 get() {
-    [[ -n $JGET_SRC ]] || { git clone https://github.com/Keitokuch/jget.sh.git ; JGET_SRC=jget ; }
+    [[ -n $JGET_SRC ]] || { JGET_SRC=jget_src; git clone https://github.com/Keitokuch/jget.sh.git $JGET_SRC; }
     [[ -f $BIN/jget ]] && rm $BIN/jget
     [[ -d $BIN/jget ]] && rm -rf $BIN/jget
     cp -f $JGET_SRC/jget.sh $BIN/jget
