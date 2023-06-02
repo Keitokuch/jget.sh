1cfac4dd6022bf601ca34d3838a9f5150d17695a
JC <keitokuch@gmail.com>
2023-06-02 17:43:05 -0400

    Fix lazygit download

--- jget_lib/formula/lazygit.sh
+++ jget_lib/formula/lazygit.sh
@@ -1,11 +1,25 @@
-VERSION=0.35
+VERSION=0.38.0
 
 exists() {
   has_command lazygit
 }
 
+
 get() {
+    version=${version:-$NVIM_VERSION}
+    case $ostype in
+        linux)
+            _get_linux
+            return
+            ;;
+        *)
+            return $ERR_SUPPORT
+            ;;
+    esac
+}
+
+_get_linux() {
     local version=${version:-$VERSION}
-    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${version}_Linux_x86_64.tar.gz"
-    tar xzf lazygit.tar.gz -C $BIN lazygit
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${version}/lazygit_${version}_Linux_x86_64.tar.gz" || return $ERR_NETWORK
+    tar xf lazygit.tar.gz -C $BIN lazygit
 }
